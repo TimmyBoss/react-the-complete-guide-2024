4ecff08b915a44c59a6d9d974db1595888884952
Tim Harte <ttjharte@ziggo.nl>
2024-08-24 09:57:42 +0200

    Added CoreConcepts reusable component

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -22,11 +23,34 @@ function Header() {
   )
 }
 
+function CoreConcept(props) {
+  return <li>
+    <img src={props.image} alt={props.title} />
+    <h3>{props.title}</h3>
+    <p>{props.description}</p>
+  </li>
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+              title="Components" 
+              description="The core UI building block." 
+              image={componentsImg}
+            />
+            <CoreConcept 
+              title="Props"  
+            />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
