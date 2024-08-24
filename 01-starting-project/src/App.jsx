35beaaa7b33d57de3ecb039e2c008e476616cd3e
Tim Harte <ttjharte@ziggo.nl>
2024-08-24 10:06:43 +0200

    Refactored CoreConcepts component by using destructuring props and the spread operator to pass props

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -23,11 +23,11 @@ function Header() {
   )
 }
 
-function CoreConcept(props) {
+function CoreConcept({ image, title, description }) {
   return <li>
-    <img src={props.image} alt={props.title} />
-    <h3>{props.title}</h3>
-    <p>{props.description}</p>
+    <img src={image} alt={title} />
+    <h3>{title}</h3>
+    <p>{description}</p>
   </li>
 }
 
@@ -39,16 +39,10 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept 
-              title="Components" 
-              description="The core UI building block." 
-              image={componentsImg}
-            />
-            <CoreConcept 
-              title="Props"  
-            />
-            <CoreConcept />
-            <CoreConcept />
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />            
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
         <h2>Time to get started!</h2>
