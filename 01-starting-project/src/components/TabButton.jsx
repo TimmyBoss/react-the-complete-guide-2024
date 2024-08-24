c20b6af957dbc98aa92835df78942e6ba13d816f
Tim Harte <ttjharte@ziggo.nl>
2024-08-24 14:58:33 +0200

    Moved onclick handler out of the tabbutton into the app component

--- 01-starting-project/src/components/TabButton.jsx
+++ 01-starting-project/src/components/TabButton.jsx
@@ -1,10 +1,7 @@
-export default function TabButton({ children }) {
-    function handleClick() {
-        console.log('Hello World!');
-    }
-
-
-    return <li>
-        <button onClick={handleClick}>{children}</button>
-    </li>;
+export default function TabButton({ children, onSelect }) {
+    return (
+        <li>
+            <button onClick={onSelect}>{children}</button>
+        </li>
+    );
 }
