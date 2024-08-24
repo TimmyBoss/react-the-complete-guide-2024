e69fa415a9b52b6f283fe7320c077529ae6be568
Tim Harte <ttjharte@ziggo.nl>
2024-08-24 15:08:22 +0200

    Added selectedButton parameter to onclick handler

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -4,8 +4,9 @@ import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-  function handleSelect() {
-      console.log('Hello World - selected!');
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    console.log(selectedButton);
   }
 
   return (
@@ -24,10 +25,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Components</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton> 
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton> 
           </menu>
           Dynamic Content
         </section>
