471be4a108a3c024095f4a743f3d721029b42902
Tim Harte <ttjharte@ziggo.nl>
2024-08-24 16:17:47 +0200

    Added dynamic list for core concepts

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -35,10 +35,7 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />            
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conceptItem) => <CoreConcept key={conceptItem.title} {...conceptItem} />)}
           </ul>
         </section>
         <section id="examples">
