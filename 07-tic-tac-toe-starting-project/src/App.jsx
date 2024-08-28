5dc6d38e31180753329ca833272a0de8bef3f833
Tim Harte <ttjharte@ziggo.nl>
2024-08-28 20:33:43 +0200

    Added Player section (hardcoded)

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,29 @@
 
 function App() {
-  
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+        <li>
+          <span className="player">
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </span>
+          <button>Edit</button>
+        </li>
+        <li>
+        <span className="player">
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </span>
+          <button>Edit</button>
+        </li>
+      </ol>
 
-  return (
-    <p>Coming soon!</p>
-  )
+      GAME BOARD
+    </div>
+
+    LOG
+  </main>;
 }
 
 export default App
