b5267601790147ffa74d09c0aa8c212b34d09f25
Tim Harte <ttjharte@ziggo.nl>
2024-09-17 12:48:29 +0200

    Added some inline styling for tutorial purposes

--- 06-reactart-project/src/components/Header.jsx
+++ 06-reactart-project/src/components/Header.jsx
@@ -1,11 +1,19 @@
 import logo from "../assets/logo.png";
 import "./Header.css";
+
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p
+        style={{
+          color: "red",
+          textAlign: "left",
+        }}
+      >
+        A community of artists and art-lovers.
+      </p>
     </header>
   );
 }
