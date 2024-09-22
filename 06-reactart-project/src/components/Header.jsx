d5b8e288c3482b22c7d32b3eaa0729182dfd9a31
Tim Harte <ttjharte@ziggo.nl>
2024-09-22 08:34:12 +0200

    Added custom font

--- 06-reactart-project/src/components/Header.jsx
+++ 06-reactart-project/src/components/Header.jsx
@@ -8,10 +8,10 @@ export default function Header() {
         alt="A canvas"
         className="mb-8 w-44 h-44 object-contain"
       />
-      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800">
+      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
         ReactArt
       </h1>
-      <p>A community of artists and art-lovers.</p>
+      <p className="text-stone-500">A community of artists and art-lovers.</p>
     </header>
   );
 }
